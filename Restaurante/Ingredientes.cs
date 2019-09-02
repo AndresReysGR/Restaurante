dbffd0f82d453cf2b8cb4a3bc8cfca6e3f3bf53d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-02 12:53:02 -0700

    Agregar archivos de proyecto.

---
+++ Restaurante/Ingredientes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante
+{
+    class Ingredientes
+    {
+        public string Nombre { get; set; }
+        public string Identificador { get; set; }
+
+        public Ingredientes ()
+        {
+            Nombre = "No Asignado";
+            Identificador = "No Asignado";
+        }
+        public Ingredientes(string nombre, string identificador)
+        {
+            Nombre = nombre;
+            Identificador = identificador;
+        }
+
+    }
+}
---
+++ Restaurante/Platillos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante
+{
+    class Platillos
+    {
+        public string Nombre { get; set; }
+        public string Identifador { get; set; }
+        public string Descripcion { get; set; }
+        public int _precio;
+        public List<Ingredientes> Ingredientes;
+
+        public int Precio
+        {
+            get
+            {
+                return _precio;
+            }
+            set
+            {
+                if(value>=50)
+                {
+                    _precio = value;
+                }
+                else
+                {
+                    throw new Exception("No es mayor de 50");
+                }
+            }
+        }
+        public Platillos()
+        {
+            Nombre = "No Asignado";
+            Identifador = "No Asignado";
+            Descripcion = "No Asignado";
+            Ingredientes = new List<Ingredientes>();
+        }
+        public Platillos(string nombre)
+        {
+            Nombre = nombre;
+            Identifador = "No Asignado";
+            Descripcion = "No Asignado";
+            Ingredientes = new List<Ingredientes>();
+
+        }
+
+
+    }
+}
